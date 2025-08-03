2a3ce767a8ebd9a7f6853a92ef139fad905d706f
tanahiro2010 <herentongkegu087@gmail.com>
2025-08-03 14:11:03 +0900

    refactor: move bot online message after startBot

--- main.ts
+++ main.ts
@@ -5,30 +5,25 @@ import { BOT_ID, BOT_TOKEN } from "./configs.ts";
 
 // botのクライアント作成
 const bot = createBot({
-    token: BOT_TOKEN,
-    botId: BOT_ID,
-    intents: Intents.Guilds | Intents.GuildMessages | Intents.MessageContent,
-    events: {
-        ready: async (_bot, payload) => {
-            console.log(`√ ${payload.user.username} is ready`);
-
-            await _bot.helpers.sendMessage(1324788358404247714n, {
-                content: `Bot is online!`,
-            });
-        },
-
-        guildMemberAdd: async (bot, member) => {
-            try {
-                await bot.helpers.sendMessage(1386242996399378462n, {
-                    content: `🎉 <@${member.id}> さん、ようこそ！`,
-                });
-            } catch (error) { 
-                console.error("Guild Member Add Error:", error);
-            }
-        },
+  token: BOT_TOKEN,
+  botId: BOT_ID,
+  intents: Intents.Guilds | Intents.GuildMessages | Intents.MessageContent,
+  events: {
+    ready: (_bot, payload) => {
+      console.log(`√ ${payload.user.username} is ready`);
     },
-});
 
+    guildMemberAdd: async (bot, member) => {
+      try {
+        await bot.helpers.sendMessage(1386242996399378462n, {
+          content: `🎉 <@${member.id}> さん、ようこそ！`,
+        });
+      } catch (error) {
+        console.error("Guild Member Add Error:", error);
+      }
+    },
+  },
+});
 
 // コマンド一覧取得
 registerPingCommand(bot);
@@ -38,8 +33,10 @@ console.log("√ Command registered");
 
 await startBot(bot);
 
-
+await bot.helpers.sendMessage(1324788358404247714n, {
+  content: `Bot is online!`,
+});
 // Deno cron job to keep the bot active
 Deno.cron("KeepAwake", "*/3 * * * *", () => {
-   console.log("√ Bot is active again!");
+  console.log("√ Bot is active again!");
 });

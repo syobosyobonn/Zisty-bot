c67920491b98cb5501e63d7adeada91b1da13db2
tanahiro2010 <herentongkegu087@gmail.com>
2025-08-03 13:45:50 +0900

    Refactor: streamline hasPermission function and improve role logging

--- utils/check.ts
+++ utils/check.ts
@@ -1,14 +1,17 @@
-import { Member, Role } from "../deps.ts";
+import { Member } from "../deps.ts";
 import { Config } from "../configs.ts";
 
 type PermissionLevel = "mod" | "team" | "member";
 
 export function hasPermission(member: Member): PermissionLevel | null {
-  if (member.roles.includes(Config.role.mod)) {
-    return "mod";
-  }
-  if (member.roles.includes(Config.role.team)) {
-    return "team";
-  }
-  return "member";
+    const memberRoles: BigInt[] = member.roles || [];
+    console.log("Member Roles:", memberRoles);
+
+    if (memberRoles.includes(Config.role.mod)) {
+        return "mod";
+    }
+    if (memberRoles.includes(Config.role.team)) {
+        return "team";
+    }
+    return "member";
 }

f04fe00b0de19b5075d03d8d19c54c27ed977ba6
Syobosyobonn <154320513+syobosyobonn@users.noreply.github.com>
2025-08-17 15:46:47 +0900

    fix: [TEST] direct-running -> "deno task"

--- runbot.sh
+++ runbot.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
-# run_main.sh
-deno run \
-  --unstable-cron \
-  --allow-env \
-  --allow-import \
-  --allow-read \
-  --allow-net \
-  --allow-write \
-  ./main.ts
+deno task start
